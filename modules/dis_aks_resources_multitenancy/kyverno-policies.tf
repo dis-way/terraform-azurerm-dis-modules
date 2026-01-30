9899e30e6375dda978cf5eaeeb504a9f623d2791
Bengt Fredh <32734566+bengtfredh@users.noreply.github.com>
2026-01-30 14:50:29 +0100

    feat: Add Kyverno FluxConfiguration resources

---
+++ modules/dis_aks_resources_multitenancy/kyverno-policies.tf
@@ -0,0 +1,34 @@
+resource "azapi_resource" "kyverno_policies" {
+  type      = "Microsoft.KubernetesConfiguration/fluxConfigurations@2024-11-01"
+  name      = "kyverno-policies"
+  parent_id = var.azurerm_kubernetes_cluster_id
+  body = {
+    properties = {
+      kustomizations = {
+        kyverno-policies = {
+          force                  = false
+          path                   = "./multitenancy/"
+          prune                  = false
+          retryIntervalInSeconds = 300
+          syncIntervalInSeconds  = 300
+          timeoutInSeconds       = 300
+          wait                   = true
+        }
+      }
+      ociRepository = {
+        insecure = false
+        repositoryRef = {
+          tag = var.flux_release_tag
+        }
+        syncIntervalInSeconds = 300
+        timeoutInSeconds      = 300
+        url                   = "oci://altinncr.azurecr.io/manifests/infra/kyverno-policies"
+        useWorkloadIdentity   = true
+      }
+      namespace                  = "platform-system"
+      reconciliationWaitDuration = "PT5M"
+      waitForReconciliation      = true
+      sourceKind                 = "OCIRepository"
+    }
+  }
+}
