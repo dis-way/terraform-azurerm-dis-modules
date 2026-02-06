cb7453e1b9270437d517d124cf1f5b43763b209f
tjololo <1145298+tjololo@users.noreply.github.com>
2026-02-06 13:00:29 +0100

    feat!: add deployment of traefik to the multitenancy cluster
    Breaking Change: Added some extra mandatory variables without defaults

--- modules/dis_aks_resources_multitenancy/variables.tf
+++ modules/dis_aks_resources_multitenancy/variables.tf
@@ -22,3 +22,23 @@ variable "aks_vnet_ipv6_cidr" {
   type        = string
   description = "AKS VNet IPv6 CIDR"
 }
+
+variable "aks_node_resource_group" {
+  type        = string
+  description = "AKS node resource group"
+}
+
+variable "aks_public_ipv4_address" {
+  type = string
+  description = "The public IPv4 address of the AKS cluster"
+}
+
+variable "aks_public_ipv6_address" {
+  type = string
+  description = "The public IPv6 address of the AKS cluster"
+}
+
+variable "default_gateway_hostname" {
+  type = string
+  description = "The default gateway hostname of the AKS cluster. This will be the Hostname in the default gateway https://gateway-api.sigs.k8s.io/reference/spec/#hostname"
+}
