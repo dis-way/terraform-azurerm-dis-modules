d3d0521e028fb55d99680a22af293c8bf61f506e
Bengt Fredh <32734566+bengtfredh@users.noreply.github.com>
2026-03-06 12:17:55 +0100

    chore: Align Terraform variable attribute formatting

--- modules/dis_aks_resources_adminservices/variables-dis-identity.tf
+++ modules/dis_aks_resources_adminservices/variables-dis-identity.tf
@@ -25,10 +25,10 @@ variable "enable_dis_identity_operator" {
 }
 
 variable "dis_identity_target_tenant_id" {
-  type = string
+  type        = string
   description = "Tenant ID where dis-identity ApplicationIdentity will be created"
-  sensitive = true
-  default = ""
+  sensitive   = true
+  default     = ""
   validation {
     condition     = var.enable_dis_identity_operator == false || (var.enable_dis_identity_operator == true && length(var.dis_identity_target_tenant_id) > 0)
     error_message = "You must provide a value for dis_identity_target_tenant_id when enable_dis_identity_operator is true."
--- modules/dis_aks_resources_multitenancy/variables-opencost.tf
+++ modules/dis_aks_resources_multitenancy/variables-opencost.tf
@@ -1,6 +1,6 @@
 variable "enable_opencost" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "Enable opencost"
 }
 
@@ -8,7 +8,7 @@ variable "opencost_tenant_id" {
   type        = string
   sensitive   = true
   description = "Tenant id where the azure monitoring workspace is deployed"
-  default = ""
+  default     = ""
   validation {
     condition     = var.enable_opencost ? length(var.opencost_tenant_id) > 0 : true
     error_message = "opencost_tenant_id must be provided when enable_opencost is true."
@@ -18,7 +18,7 @@ variable "opencost_tenant_id" {
 variable "opencost_azure_monitoring_workspace_id" {
   type        = string
   description = "Azure id of the monitoring workspace to read from"
-  default = ""
+  default     = ""
   validation {
     condition     = var.enable_opencost ? length(var.opencost_azure_monitoring_workspace_id) > 0 : true
     error_message = "opencost_azure_monitoring_workspace_id must be provided when enable_opencost is true."
@@ -28,7 +28,7 @@ variable "opencost_azure_monitoring_workspace_id" {
 variable "opencost_prometheus_endpoint" {
   type        = string
   description = "URL of the prometheus endpint that opencost queries"
-  default = ""
+  default     = ""
   validation {
     condition     = var.enable_opencost ? length(var.opencost_prometheus_endpoint) > 0 : true
     error_message = "opencost_prometheus_endpoint must be provided when enable_opencost is true."
