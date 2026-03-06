6e1b28e35bb6558bbf5dfaba141f9631e74fbaf0
Vemund Gaukstad <tjololo@users.noreply.github.com>
2026-03-06 11:32:50 +0100

    feat: add resources needed to deploy opencost (#68)
    * feat: add resources needed to deploy opencost
    
    * more permissive azurerm version
    
    ---------
    
    Co-authored-by: tjololo <1145298+tjololo@users.noreply.github.com>

---
+++ modules/dis_aks_resources_multitenancy/opencost.tf
@@ -0,0 +1,68 @@
+resource "azurerm_user_assigned_identity" "opencost_metrics_reader" {
+  count               = var.enbale_opencost ? 1 : 0
+  name                = "opencost-reader"
+  resource_group_name = var.aks_resource_group
+  location            = "norwayeast"
+}
+
+resource "azurerm_federated_identity_credential" "opencost_metrics_reader" {
+  count               = var.enbale_opencost ? 1 : 0
+  name                = "opencost-aks-federation"
+  resource_group_name = azurerm_user_assigned_identity.opencost_metrics_reader[0].resource_group_name
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = var.azurerm_kubernetes_cluster_oidc_issuer_url
+  subject             = "system:serviceaccount:opencost-system:opencost"
+  parent_id           = azurerm_user_assigned_identity.opencost_metrics_reader[0].id
+}
+
+resource "azurerm_role_assignment" "opencost_metrics_reader" {
+  count                = var.enbale_opencost ? 1 : 0
+  principal_id         = azurerm_user_assigned_identity.opencost_metrics_reader[0].client_id
+  scope                = var.opencost_azure_monitoring_workspace_id
+  role_definition_name = "Monitoring Reader"
+}
+
+resource "azapi_resource" "otel_operator" {
+  type      = "Microsoft.KubernetesConfiguration/fluxConfigurations@2024-11-01"
+  name      = "opencost"
+  parent_id = var.azurerm_kubernetes_cluster_id
+  body = {
+    properties = {
+      kustomizations = {
+        opencost = {
+          force = false
+          path  = "./multitenancy"
+          postBuild = {
+            substitute = {
+              AZURE_TENANT_ID : "${var.opencost_tenant_id}"
+              OPENCOST_CLIENT_ID : "${azurerm_user_assigned_identity.opencost_metrics_reader[0].client_id}"
+              AZURE_PROMETHEUS_ENDPOINT : "${var.opencost_prometheus_endpoint}"
+              AKS_VNET_IPV4_CIDR : "${var.aks_vnet_ipv4_cidr}"
+              AKS_VNET_IPV6_CIDR : "${var.aks_vnet_ipv6_cidr}"
+            }
+          }
+          prune                  = false
+          retryIntervalInSeconds = 300
+          syncIntervalInSeconds  = 300
+          timeoutInSeconds       = 300
+          wait                   = true
+        }
+      }
+      ociRepository = {
+        insecure = false
+        repositoryRef = {
+          tag = var.flux_release_tag
+        }
+        syncIntervalInSeconds = 300
+        timeoutInSeconds      = 300
+        url                   = "oci://altinncr.azurecr.io/manifests/infra/opencost"
+        useWorkloadIdentity   = true
+      }
+      namespace                  = "platform-system"
+      reconciliationWaitDuration = "PT5M"
+      waitForReconciliation      = true
+      sourceKind                 = "OCIRepository"
+    }
+  }
+}
+
---
+++ modules/dis_aks_resources_multitenancy/variables-opencost.tf
@@ -0,0 +1,36 @@
+variable "enable_opencost" {
+  type = bool
+  default = false
+  description = "Enable opencost"
+}
+
+variable "opencost_tenant_id" {
+  type        = string
+  sensitive   = true
+  description = "Tenant id where the azure monitoring workspace is deployed"
+  default = ""
+  validation {
+    condition     = var.enable_opencost ? length(var.opencost_tenant_id) > 0 : true
+    error_message = "opencost_tenant_id must be provided when enable_opencost is true."
+  }
+}
+
+variable "opencost_azure_monitoring_workspace_id" {
+  type        = string
+  description = "Azure id of the monitoring workspace to read from"
+  default = ""
+  validation {
+    condition     = var.enable_opencost ? length(var.opencost_azure_monitoring_workspace_id) > 0 : true
+    error_message = "opencost_azure_monitoring_workspace_id must be provided when enable_opencost is true."
+  }
+}
+
+variable "opencost_prometheus_endpoint" {
+  type        = string
+  description = "URL of the prometheus endpint that opencost queries"
+  default = ""
+  validation {
+    condition     = var.enable_opencost ? length(var.opencost_prometheus_endpoint) > 0 : true
+    error_message = "opencost_prometheus_endpoint must be provided when enable_opencost is true."
+  }
+}
