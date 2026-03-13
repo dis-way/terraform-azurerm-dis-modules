07a570658908b2d2dc7c175ecab312129e92462f
tjololo <1145298+tjololo@users.noreply.github.com>
2026-03-13 14:32:04 +0100

    feat: setup rbac permissions for the product team groups

--- modules/dis_products_syncroot_multitenancy/providers.tf
+++ modules/dis_products_syncroot_multitenancy/providers.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0.0"
+    }
     azapi = {
       source  = "Azure/azapi"
       version = ">= 2.3.0"
