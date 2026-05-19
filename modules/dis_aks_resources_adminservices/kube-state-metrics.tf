587164ff675555a748a9306a30054f419851f0be
Bengt Fredh <32734566+bengtfredh@users.noreply.github.com>
2026-05-19 10:29:19 +0200

    feat: Add kube-state-metrics Flux configuration

---
+++ modules/dis_aks_resources_adminservices/kube-state-metrics.tf
@@ -0,0 +1,34 @@
+resource "azapi_resource" "kube_state_metrics" {
+  type      = "Microsoft.KubernetesConfiguration/fluxConfigurations@2024-11-01"
+  name      = "kube-state-metrics"
+  parent_id = var.azurerm_kubernetes_cluster_id
+  body = {
+    properties = {
+      kustomizations = {
+        kube-state-metrics = {
+          force                  = false
+          path                   = "./"
+          prune                  = false
+          retryIntervalInSeconds = 300
+          syncIntervalInSeconds  = 300
+          timeoutInSeconds       = 300
+          wait                   = false
+        }
+      }
+      ociRepository = {
+        insecure = false
+        repositoryRef = {
+          tag = var.flux_release_tag
+        }
+        syncIntervalInSeconds = 300
+        timeoutInSeconds      = 300
+        url                   = "oci://altinncr.azurecr.io/manifests/infra/kube-state-metrics"
+        useWorkloadIdentity   = true
+      }
+      namespace                  = "flux-system"
+      reconciliationWaitDuration = "PT5M"
+      waitForReconciliation      = false
+      sourceKind                 = "OCIRepository"
+    }
+  }
+}
