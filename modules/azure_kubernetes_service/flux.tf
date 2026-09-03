8eda45571db18b6ba256f07b9367c01a2269d211
Vemund Gaukstad <tjololo@users.noreply.github.com>
2026-09-03 15:28:01 +0200

    feat: allow caller of azure_kubernetes_service module to opt out of deploying flux through azure extension (#132)
    needed to support new clusters where we deploy flux with the flux operator
    
    Co-authored-by: tjololo <1145298+tjololo@users.noreply.github.com>

--- modules/azure_kubernetes_service/flux.tf
+++ modules/azure_kubernetes_service/flux.tf
@@ -1,4 +1,5 @@
 resource "azurerm_kubernetes_cluster_extension" "flux" {
+  count          = var.enable_flux ? 1 : 0
   name           = "flux"
   cluster_id     = azurerm_kubernetes_cluster.aks.id
   extension_type = "microsoft.flux"
