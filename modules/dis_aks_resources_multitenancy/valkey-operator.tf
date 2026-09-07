b2f9da4c9aff5f2d4e389f931a5ea857baec5385
Sebastian Duran <duran.c.sebastian@gmail.com>
2026-09-07 14:13:30 +0200

    feat(multitenancy): add gated valkey-operator flux configuration (#134)

---
+++ modules/dis_aks_resources_multitenancy/valkey-operator.tf
@@ -0,0 +1,35 @@
+resource "azapi_resource" "valkey_operator" {
+  count     = var.enable_valkey_operator ? 1 : 0
+  type      = "Microsoft.KubernetesConfiguration/fluxConfigurations@2024-11-01"
+  name      = "valkey-operator"
+  parent_id = var.azurerm_kubernetes_cluster_id
+  body = {
+    properties = {
+      kustomizations = {
+        valkey-operator = {
+          force                  = false
+          path                   = "./multitenancy/"
+          prune                  = false
+          retryIntervalInSeconds = 300
+          syncIntervalInSeconds  = 300
+          timeoutInSeconds       = 300
+          wait                   = true
+        }
+      }
+      ociRepository = {
+        insecure = false
+        repositoryRef = {
+          tag = var.flux_release_tag
+        }
+        syncIntervalInSeconds = 300
+        timeoutInSeconds      = 300
+        url                   = "oci://altinncr.azurecr.io/manifests/infra/valkey-operator"
+        useWorkloadIdentity   = true
+      }
+      namespace                  = "platform-system"
+      reconciliationWaitDuration = "PT5M"
+      waitForReconciliation      = true
+      sourceKind                 = "OCIRepository"
+    }
+  }
+}
---
+++ modules/dis_aks_resources_multitenancy/variables-valkey-operator.tf
@@ -0,0 +1,5 @@
+variable "enable_valkey_operator" {
+  type        = bool
+  default     = false
+  description = "Enable the Valkey operator in the cluster."
+}
